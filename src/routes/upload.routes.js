33b9f3604679efc9814f031a1bf09f7e130e3f7c
Radiat09 <radiathossainr@gmail.com>
2024-12-30 00:48:52 +0600

    latest code

--- src/routes/upload.routes.js
+++ src/routes/upload.routes.js
@@ -6,7 +6,7 @@ import { uploadPDFv3 } from "../controllers/upload.controller.js";
 const router = express.Router();
 // const upload = multer({ dest: "uploads/" });
 const storage = multer.memoryStorage();
-const upload = multer({ storage });
+const upload = multer({ storage, limits: { fileSize: 50 * 1024 * 1024 } });
 
 router.use(authenticateToken);
 router.post("/", upload.single("file"), uploadPDFv3);
